ddb845122fd8622503709680fec944044e2d0a83
bruno.krebs <krebs.bruno@gmail.com>
2017-05-29 10:24:12 -0300

    Home made authorization

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreAuth.Models
+{
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+    {
+      Database.EnsureCreated();
+    }
+  }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DotNetCoreAuth.Models
+{
+  public class ApplicationUser : IdentityUser
+  {
+  }
+}
